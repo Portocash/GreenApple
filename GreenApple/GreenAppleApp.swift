6a0fcd4a2948078444c466d0d0b0ef302beb77fc
GreenApple <c.portosalvo@icloud.com>
2023-02-28 11:19:54 +0100

    Initial Commit

---
+++ GreenApple/GreenAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenAppleApp.swift
+//  GreenApple
+//
+//  Created by carlo on 28/02/23.
+//
+
+import SwiftUI
+
+@main
+struct GreenAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
